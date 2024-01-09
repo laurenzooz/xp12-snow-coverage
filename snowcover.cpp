9f2dd3c475dc65c891b84d3bebd1b2d6d6e30143
laurenzooz <lauri.pahkala@hotmail.com>
2024-01-09 13:17:23 +0200

    Use current hour and current location

--- snowcover.cpp
+++ snowcover.cpp
@@ -4,36 +4,24 @@
 #include "XPLMUtilities.h"
 
 #include <string>
+#include <cstring>
 
 #include "httplib.h"
 #include <nlohmann/json.hpp>
 
-double last_depth = 1.25;
+static float last_depth = 1.25;
 
 
 
 
-double translate_units(double depth_m)
+float translate_units(float depth_m)
 {
-/*
-	return 0;
-	std::cout << "converitn\n";
 	if (depth_m > 0.5) {
 		return 0;
-	}
-	else if (depth_m > 0) {
-		return 0.3;
-	} else {
-		return 1.25;
-	}
-*/
-	std::cout << "converitn\n";
-	if (depth_m > 0.5) {
-		return 0;
-	} else if (depth_m > 0.15) {
-		return ( (1 / (10 * depth_m)) - 0.1 - 0.2 * depth_m);
+	} else if (depth_m > 0.05) {
+		return ( (1 / (20 * depth_m)) - 0.2 + 0.2 * depth_m);
 	} else if (depth_m > 0) {
-		return (-7.1 * depth_m + 1.3);
+		return (-9 * depth_m + 1.25);
 	}	
 	else {
 		return 1.25;
@@ -42,59 +30,75 @@ double translate_units(double depth_m)
 }
 
 
+bool initialized = false;
+
+static XPLMDataRef lat = XPLMFindDataRef("sim/flightmodel/position/latitude");
+static XPLMDataRef lon = XPLMFindDataRef("sim/flightmodel/position/longitude");
 
 
-
-static float get_depth(float time_since_last_call, float time_since_last_floop, int floop_counter, void* inRefcon) 
+static float set_depth(float time_since_last_call, float time_since_last_floop, int floop_counter, void* inRefcon) 
 {	
-	std::cout << "gettin depth\n";
+	//std::cout << "settin depth\n";
+	if (initialized){
+	
 
-	XPLMDataRef lat = XPLMFindDataRef("sim/flightmodel/position/latitude");
-	XPLMDataRef lon = XPLMFindDataRef("sim/flightmodel/position/longitude");
+	
 
-	if (lat){
+	//if (lat && lon && initialized){
 		httplib::Client cli("api.open-meteo.com");
-    	auto res = cli.Get("/v1/forecast?latitude=62.52&longitude=23.409994&hourly=snow_depth&forecast_days=1");
-		//std::cout << res;
+    	auto res = cli.Get("/v1/forecast?latitude=" + std::to_string(XPLMGetDataf(lat)) + "&longitude=" + std::to_string(XPLMGetDataf(lon)) + "&hourly=snow_depth&forecast_days=1");
+    	//auto res = cli.Get("/v1/forecast?latitude=62.52&longitude=23.409994&hourly=snow_depth&forecast_days=1");
+
     	if (res && res->status == 200) {
         	
-			auto json = nlohmann::json::parse(res->body);
-			auto hourly = json["hourly"];
-    		auto snow_depth = hourly["snow_depth"];
+			nlohmann::json j = nlohmann::json::parse(res->body);
 
-/*
+
+
 			// get the current hour
 			auto now = std::chrono::system_clock::now();
   			std::time_t t_c = std::chrono::system_clock::to_time_t(now);
   			std::tm* utc_tm = std::gmtime(&t_c);
   			int hour = utc_tm->tm_hour;
-  			std::cout << "The current hour in UTC is: " << hour << std::endl; 
 
-			if (hour < 0)  { hour = 0; } else if (hour >= snow_depth.size()) { hour = snow_depth.size() - 1; }
-	*/		//last_depth = translate_units(snow_depth[10]);
-			if (snow_depth.size() > 0) { last_depth = translate_units(snow_depth[10]); } else { return 120; }
+			if (hour < 0)  { hour = 0; } else if (hour > 23) { hour = 23; }
+			//last_depth = translate_units(snow_depth[10]);
+			float depth = j["hourly"]["snow_depth"][hour].get<float>();
+			
 			//last_depth = 0;
-			std::cout << "Snow depth: " << snow_depth[10] << " xp value: " <<  last_depth << std::endl;
+			if (!std::isnan(depth)) {
+				last_depth = translate_units(depth);
+
+
+			}
     	} else {
-			std::cout << "error\n";
+			XPLMDebugString("Snow data not found\n");
 		}
-	} 
-    return 120;
+	//} 
+	}
+    return 600;
 }
-bool initialized = false;
+
+
+
 static float floop_cb(float time_since_last_call, float time_since_last_floop, int floop_counter, void* inRefcon) 
 {
 	XPLMDataRef snowRef = XPLMFindDataRef("sim/private/controls/wxr/snow_now");
-        if (snowRef)
-        {
+
+        //if (initialized)
+        //{
+		if (XPLMGetDataf(lat) != 0.0 && XPLMGetDataf(lon) != 0.0) {
+			//std::cout << ("snowref is\n");
 			if (!initialized) {
 				initialized = true;
-				get_depth(0, 0, 0, NULL);
+				set_depth(0, 0, 0, NULL);
 				std::cout << "initialized\n";
 			}
-            XPLMSetDataf(snowRef, last_depth); 
-			
-        }
+			XPLMSetDataf(snowRef, last_depth); 
+
+            
+		}
+        //}
     return -1;
 }
 
@@ -109,13 +113,17 @@ PLUGIN_API int XPluginStart(
 	
     
     XPLMRegisterFlightLoopCallback(floop_cb, -1, NULL); // flight loop every cycle
-    XPLMRegisterFlightLoopCallback(get_depth, 120, NULL); // update snow depth every 5 mins
+    XPLMRegisterFlightLoopCallback(set_depth, 600, NULL); // update snow depth every 5 mins
+
+	XPLMDebugString("starting snowcover plugin\n");
 
     return 1;
 }
 
 PLUGIN_API void	XPluginStop(void) 
 { 
+	XPLMUnregisterFlightLoopCallback(floop_cb, NULL);
+	XPLMUnregisterFlightLoopCallback(set_depth, NULL);
 	
 }
 
@@ -128,6 +136,6 @@ PLUGIN_API void XPluginReceiveMessage(
     int				inMessage,
     void *			inParam)
 {
-    
+
 }
 
